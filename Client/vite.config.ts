34f2295f36cf4c59f5375765496a0476f101dc69
Sagar Vishwakarma <sv472921@gmail.com>
2025-01-06 15:49:02 +0530

    commit

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-      "/api/user" : "http://localhost:8000"
+      "/api/user" : "https://smarttalk-chatbot-app.onrender.com"
     }
   },
   plugins: [react()],
